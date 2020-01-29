792fd6fa20ecc4cbbd3c3ff407b1686fc7d414c5
Alay Majmudar <alaymajmudar.loyola@gmail.com>
2020-01-28 21:04:16 -0500

    1) Applying Adaboost algorithm to Terrain dataset. 2) Minor update in Decision Tree code.

---
+++ Codes/Adaboost.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 28 10:16:29 2020
+
+@author: Admin
+"""
+#!/usr/bin/python3
+
+import matplotlib.pyplot as plt
+from prep_terrain_data import makeTerrainData
+from class_vis import prettyPicture
+from sklearn.ensemble import AdaBoostClassifier
+features_train, labels_train, features_test, labels_test = makeTerrainData()
+from sklearn.metrics import accuracy_score
+
+### the training data (features_train, labels_train) have both "fast" and "slow" points mixed
+### in together--separate them so we can give them different colors in the scatterplot,
+### and visually identify them
+grade_fast = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==0]
+bumpy_fast = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==0]
+grade_slow = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==1]
+bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==1]
+
+
+clf = AdaBoostClassifier(base_estimator='SVM',n_estimators=100, random_state=0)
+clf.fit(features_train, labels_train)
+
+
+pred = clf.predict(features_test)
+
+print(accuracy_score(pred,labels_test))
+
+#### initial visualization
+plt.xlim(0.0, 1.0)
+plt.ylim(0.0, 1.0)
+plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
+plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
+plt.legend()
+plt.xlabel("bumpiness")
+plt.ylabel("grade")
+plt.show()
+#################################################################################
+
+
+
+
+
+### your code here!  name your classifier object clf if you want the 
+### visualization code (prettyPicture) to show you the decision boundary
+
+
+
+
+
+
+
+
+try:
+    prettyPicture(clf, features_test, labels_test)
+except NameError:
+    pass
+
