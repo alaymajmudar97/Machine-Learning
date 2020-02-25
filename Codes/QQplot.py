d7f9aa704cd9909e03bb1f9b9d7eeb1792cee732
Alay Majmudar <alaymajmudar.loyola@gmail.com>
2020-02-25 17:54:06 -0500

    Learning THe concept of Dimensionality Reduction. Working with MNIST DATA

--- Codes/QQplot.py
+++ Codes/QQplot.py
@@ -5,3 +5,49 @@ Created on Mon Feb 17 21:42:58 2020
 @author: Admin
 """
 
+
+import time
+start_time = time.time()
+for i in range(0,5):
+    
+    print(i)
+    time.sleep(5)
+endtime = time.time()
+
+print(endtime-start_time)   
+
+
+
+a = ['adsfasdfasdfasdf']
+b = a.copy()
+for i in range(0,3):
+    b[0] = 'alay ' + b[0]
+    for i in range(0,4):
+        print(b)
+    b = a.copy()
+    print(b)
+
+
+
+
+a= [1,2,3,4]
+b = a
+b.append(5)
+print(a)
+print(b)
+
+b =a.copy()
+
+b.append(7)
+a
+b
+
+
+
+
+def test():
+    a =7
+    b = 5 +a
+    print(a)
+    
+test()
---
+++ Codes/dimensionality_reduction.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 25 01:03:29 2020
+
+@author: Admin
+"""
+
+import numpy as np
+import pandas as pd
+
+
+mnist = pd.read_csv()
